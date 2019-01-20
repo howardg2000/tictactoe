c54e35ea1e584c9414a6c0cbfd7ab1283317203c
Howie Guo <hjguo@Howies-MacBook-Pro.local>
2019-01-20 15:48:35 -0500

    undo feature

--- application.py
+++ application.py
@@ -13,18 +13,20 @@ Session(app)
 def index():
 
     if "board" not in session:
-        session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+        session["board"] = [[None, None, None], [None, None, None], [None, None, None]] * 9
         session["turn"] = "X"
+        session["move"] = 0
         session["playing"] = True
     
-    return render_template("game.html", game=session["board"], turn=session["turn"], playing=session["playing"])
+    return render_template("game.html", game=session["board"][session["move"]], turn=session["turn"], playing=session["playing"])
 
 @app.route("/play/<int:row>/<int:col>")
 def play(row, col):
     
-    session["board"][row][col] = session["turn"]
+    session["move"] += 1
+    session["board"][session["move"]][row][col] = session["turn"]
 
-    game = session["board"]
+    game = session["board"][session["move"]]
 
     if game[row][0] == game[row][1] == game[row][2] or game[0][col] == game[1][col] == game[2][col] or checkDiagonals():
         session["playing"] = False
@@ -41,6 +43,7 @@ def reset():
     return redirect(url_for("index"))
 
 def checkDiagonals():
-    if session["board"][1][1]:
-        return session["board"][0][0]==session["board"][1][1]==session["board"][2][2] or session["board"][2][0]==session["board"][1][1]==session["board"][0][2]
+    game = session["board"][session["move"]]
+    if game[1][1]:
+        return game[0][0]==game[1][1]==game[2][2] or game[2][0]==game[1][1]==game[0][2]
     return False
